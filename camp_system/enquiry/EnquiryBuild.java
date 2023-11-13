9e8f489128512a5dd4e022dc9f7aded774e8988e
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-13 21:38:04 +0800

    User will be automatically be a part of the parameter

--- camp_system/enquiry/EnquiryBuild.java
+++ camp_system/enquiry/EnquiryBuild.java
@@ -7,21 +7,14 @@ import camp_system.enquiry.Enquiry;
 public class EnquiryBuild {
     public Enquiry build(User user){
         Scanner scan = new Scanner(System.in);
-        int indexStudent = 0;
+        
         int indexCamp =0;
-        User s1 = new User();
         Camp c1 = new Camp();
-        System.out.println("Please enter the following details: ");
-        System.out.println("Your name: "); String studentName;
-
-        while(user.get(indexStudent).studentName != studentName){indexStudent++;}
-        s1 = user.get(indexStudent);
-
         System.out.println("Camp name: "); String campName;
         while(camp.get(indexCamp).campName != campName) {indexCamp++;}
         c1 = camp.get(indexCamp);
         System.out.println("Question: "); String question = scan.nextLine();
         
-        return new Enquiry(s1, c1, question);
+        return new Enquiry(user, c1, question);
     }
 }
