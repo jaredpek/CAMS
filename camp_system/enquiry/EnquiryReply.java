43abffd43801669d68f786b1a135d49cd80d6c62
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-13 21:18:25 +0800

    Add get point

--- camp_system/enquiry/EnquiryReply.java
+++ camp_system/enquiry/EnquiryReply.java
@@ -2,10 +2,11 @@ package camp_system.enquiry;
 
 
 public class EnquiryReply {
-    public void reply(int index){
+    public void reply(User user,int index){
         System.out.println("Enter your reply"); String reply = scan.nextLine();
         enquiries.get(index).setReply(reply);
         enquiries.get(index).setStatus(Replied);
+        user.getPoint();
     }
     
 }
