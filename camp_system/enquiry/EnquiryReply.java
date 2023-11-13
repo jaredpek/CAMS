07d66329d74996edcbbe66b32c86b768aec50440
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-13 21:22:42 +0800

    Update EnquiryReply.java

--- camp_system/enquiry/EnquiryReply.java
+++ camp_system/enquiry/EnquiryReply.java
@@ -2,7 +2,7 @@ package camp_system.enquiry;
 
 
 public class EnquiryReply {
-    public void reply(User user,int index){
+    public void reply(EnquiryControl enquiries,User user,int index){
         System.out.println("Enter your reply"); String reply = scan.nextLine();
         enquiries.get(index).setReply(reply);
         enquiries.get(index).setStatus(Replied);
