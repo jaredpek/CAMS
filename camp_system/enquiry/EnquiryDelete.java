a048aa64dc502931097a0f38f21cf6ddc5a1ea08
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-13 21:40:01 +0800

    Update EnquiryDelete.java

--- camp_system/enquiry/EnquiryDelete.java
+++ camp_system/enquiry/EnquiryDelete.java
@@ -1,7 +1,7 @@
 package camp_system.enquiry;
 
 public class EnquiryDelete{
-    public void delete(EnquiryControl global,EnquiryControl enquiries, int delete){
+    public void delete(EnquiryControl global,EnquiryControl enquiries, int index){
         global.remove(enquiries.get(index));
     }
 }
