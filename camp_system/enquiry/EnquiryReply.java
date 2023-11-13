957386b4b9283a1386986e39ce4419bdc9635c3d
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-13 12:41:42 +0800

    Update and rename EnquiryEdit.java to EnquiryReply.java

--- camp_system/enquiry/EnquiryReply.java
+++ camp_system/enquiry/EnquiryReply.java
@@ -1,8 +1,8 @@
 package camp_system.enquiry;
 
 
-public class EnquiryEdit {
-    public void edit(int index){
+public class EnquiryReply {
+    public void reply(int index){
         System.out.println("Enter your reply"); String reply = scan.nextLine();
         enquiries.get(index).setReply(reply);
         enquiries.get(index).setStatus(Replied);
