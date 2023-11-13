878fa9f3468f13e8c004e999cb747b89aad752d3
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-13 21:17:50 +0800

    Update EnquiryEdit.java

--- camp_system/enquiry/EnquiryEdit.java
+++ camp_system/enquiry/EnquiryEdit.java
@@ -4,7 +4,7 @@ package camp_system.enquiry;
 public class EnquiryEdit {
     public void edit(int index){
         System.out.println("Enter your edit"); String edit = scan.nextLine();
-        enquiries.get(index).setReply(edit);
+        enquiries.get(index).setQuestion(edit);
     }
     
 }
