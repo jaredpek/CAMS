df0bea3d97306a2fb35f29f89adeff20af41fcd3
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-19 14:43:59 +0800

    Comments

--- camp_system/enquiry/EnquiryControl.java
+++ camp_system/enquiry/EnquiryControl.java
@@ -9,14 +9,18 @@ import camp_system.camp.CampSelect;
 import camp_system.user.Role;
 import camp_system.user.User;
 
+/** Represents a control call that manages a list of enquiries */
 public class EnquiryControl implements IControl, IEnquiry {
+    /** List of all the available enquiries */
     private static ArrayList <Enquiry> enquiries = new ArrayList <Enquiry> ();
     public static EnquiryControl instance = new EnquiryControl();
 
+    /** Initialises the enquiry array with data from a CSV file */
     public static void start() {
         enquiries.addAll(EnquiryParse.parse("camp_system\\data\\enquiries.csv"));
     }
 
+     /** Saves the enquiry array to the CSV file */
     public static void close() {
         EnquiryParse.write("camp_system\\data\\enquiries.csv", enquiries);
     }
@@ -24,6 +28,7 @@ public class EnquiryControl implements IControl, IEnquiry {
     /*
      * Adds a new enquiry into the list of enquiries
      * Only available for Students
+     * @param user This is to check the user
     */
     public void add(User user) {
         ArrayList <Camp> camps = CampControl.campControl.getByGroupNotCommittee(user);
@@ -35,6 +40,7 @@ public class EnquiryControl implements IControl, IEnquiry {
 
     /* Allow Students to edit their enquiry
      * Only applicable when the enquiry is still processing
+     * @param user This is the user (student) that edits the enquiry
     */
     public void edit(User user) {
         ArrayList <Enquiry> studentEnquiries = getByStudent(user);
@@ -45,6 +51,7 @@ public class EnquiryControl implements IControl, IEnquiry {
 
     /*
      * Allow only the student to delete their enquiry
+     * @param user This is the user (student) that wants to delete the enquiry
     */
     public void delete(User user) {
         ArrayList <Enquiry> studentEnquiries = getByStudent(user);
@@ -56,6 +63,7 @@ public class EnquiryControl implements IControl, IEnquiry {
     /*
      * Only allow Committee Member or Staff to reply to the enquiry
      * Adds a point if the user is a Committee Member
+     * @param user This is the user (Committee Member) that wants to reply to the enquiry
     */
     public void reply(User user) {
         ArrayList <Camp> camps = new ArrayList <Camp> ();
@@ -71,6 +79,7 @@ public class EnquiryControl implements IControl, IEnquiry {
 
     /* 
      * Get the list of enquiry for the camp's committee member and staff
+     * @param camp This is the list of camp that the user is in charge of
     */
     public ArrayList<Enquiry> getByCamp(Camp camp) {
         ArrayList<Enquiry> campEnquiries = new ArrayList<Enquiry>();
@@ -82,6 +91,8 @@ public class EnquiryControl implements IControl, IEnquiry {
 
     /* 
      * Allow students to see their own enquiry
+     * @param student This is the user that has access to their own enquiry
+     * @return studentEnquiries This is the enquiries that the student made
     */
     public ArrayList <Enquiry> getByStudent(User student) {
         ArrayList<Enquiry> studentEnquiries = new ArrayList<Enquiry>();
