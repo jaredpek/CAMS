f77d3c167f10a19a9e6b4dec386ad8d7feefb5fc
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-19 14:37:36 +0800

    Comment

--- camp_system/enquiry/EnquiryEdit.java
+++ camp_system/enquiry/EnquiryEdit.java
@@ -2,7 +2,13 @@ package camp_system.enquiry;
 
 import camp_system.scanner.Scan;
 
+/** Represents a class that edits the enquiry */
 public class EnquiryEdit {
+    /**
+     * Obtain user input and edits the required enquiry
+     * @param enquiry This is the enquiry to edit
+     */
+     
     public static void edit(Enquiry enquiry){
         System.out.printf("New Enquiry: "); String edit = Scan.scan.nextLine();
         enquiry.setQuestion(edit);
