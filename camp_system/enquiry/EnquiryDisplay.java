457fac2d9853b4f7d668ada0c16760529971c14e
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-19 14:38:41 +0800

    Comments

--- camp_system/enquiry/EnquiryDisplay.java
+++ camp_system/enquiry/EnquiryDisplay.java
@@ -2,7 +2,12 @@ package camp_system.enquiry;
 
 import java.util.ArrayList;
 
+/** Represent a display class that is responsible for printing the enquiries */
 public class EnquiryDisplay {
+    /** 
+     * Prints the provided enquiry objects in the standadized format
+     * @param enquiries THis is the list of enquiries to print
+     */
     public static void printEnquiries(ArrayList <Enquiry> enquiries) {
         if (enquiries.size() <= 0) {
             System.out.println("No Enquiries Found");
