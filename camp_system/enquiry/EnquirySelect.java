10e242d4c80eb591fa6faebf9ef1fcada12d96c3
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-19 14:12:55 +0800

    Comments

--- camp_system/enquiry/EnquirySelect.java
+++ camp_system/enquiry/EnquirySelect.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 
 import camp_system.scanner.Scan;
 
+/** Represents a class that selects a list of enquiries */
 public class EnquirySelect {
+    /** 
+     * Prints out the list of enquiries and allow user to select the specific enquiry 
+     * @param enquiries
+     * @return Enquiry
+     */
     public static Enquiry select(ArrayList <Enquiry> enquiries) {
         EnquiryDisplay.printEnquiries(enquiries);
         if (enquiries.size() == 0) return null;
