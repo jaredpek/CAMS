81e45d7077970926bffb14f3f574d5a5c536af82
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-19 14:35:45 +0800

    Comments

--- camp_system/enquiry/EnquiryParse.java
+++ camp_system/enquiry/EnquiryParse.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 
 import camp_system.csv_parser.CSVParse;
 
+/** Represents a class that reads and writes enquiry objects to and from a CSV*/
 public class EnquiryParse {
+    /** 
+     * Create a new list of enquiry objects from a provided CSV file
+     * @param file The parth to the CSV file to read
+     * @return ArrayList of enquiry objects
+     *//
     public static ArrayList <Enquiry> parse(String file) {
         ArrayList <Enquiry> results = new ArrayList <Enquiry> ();
         try {
@@ -14,6 +20,11 @@ public class EnquiryParse {
         return results;
     }
 
+    /**
+     * Writes a list of camps to a CSV file
+     * @param file The path to write the CSV file to
+     * @param enquiries The list of enquiries to write to the CSV file
+     */
     public static void write(String file, ArrayList <Enquiry> enquiries) {
         ArrayList <String> data = new ArrayList <String> ();
         for (Enquiry enquiry : enquiries) {
