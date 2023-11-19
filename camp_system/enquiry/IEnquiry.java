5e832e70aa545d1150676ca28ccbdce7744f9980
Ken <95632405+fearyj@users.noreply.github.com>
2023-11-19 14:08:29 +0800

    Comment

--- camp_system/enquiry/IEnquiry.java
+++ camp_system/enquiry/IEnquiry.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import camp_system.camp.Camp;
 import camp_system.user.User;
 
+/** An interface that implements methods to manipulate the list of enquiries*/
 public interface IEnquiry {
     public void reply(User user);
     public ArrayList<Enquiry> getByCamp(Camp camp);
